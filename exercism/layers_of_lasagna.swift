831b97b5f6241596cb088fbeec7e9a7e08235edd
Matheus Ferreira <Mathferreiranasc12@gmail.com>
2024-02-25 17:33:26 -0300

    feat/creating exercism folder

---
+++ exercism/layers_of_lasagna.swift
@@ -0,0 +1,12 @@
+let expectedMinutesInOven = 40
+func remainingMinutesInOven(elapsedMinutes: Int) -> Int {
+    return expectedMinutesInOven - elapsedMinutes
+}
+func preparationTimeInMinutes(layers: Int) -> Int {
+    return layers * 2
+}
+func totalTimeInMinutes(layers: Int, elapsedMinutes: Int) -> Int {
+    let preparationTime = preparationTimeInMinutes(layers: layers)
+    let totalMinutes = preparationTime + elapsedMinutes
+    return totalMinutes
+}
