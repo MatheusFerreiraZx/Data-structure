c6bff104e9580f67da2aa126305490f7341667ad
Matheus Ferreira <Mathferreiranasc12@gmail.com>
2024-03-15 11:44:30 -0300

    feat: adding exercism exercise

--- exercism/layers_of_lasagna.swift
+++ exercism/layers_of_lasagna.swift
@@ -1,10 +1,13 @@
 let expectedMinutesInOven = 40
+
 func remainingMinutesInOven(elapsedMinutes: Int) -> Int {
     return expectedMinutesInOven - elapsedMinutes
 }
+
 func preparationTimeInMinutes(layers: Int) -> Int {
     return layers * 2
 }
+
 func totalTimeInMinutes(layers: Int, elapsedMinutes: Int) -> Int {
     let preparationTime = preparationTimeInMinutes(layers: layers)
     let totalMinutes = preparationTime + elapsedMinutes
